048f8422529771655c34f0097ca076c99e5ea88b
Anish Liu <jk5605012@gmail.com>
2020-08-05 17:59:25 +0800

    add gorm.model

--- models/account/account.go
+++ models/account/account.go
@@ -1,6 +1,9 @@
 package account
 
+import "github.com/jinzhu/gorm"
+
 type Accounts struct {
+	gorm.Model
 	UserName string `json:"username" binding:"required"`
 	Pwd      string `json:"pwd" binding:"required"`
 }
