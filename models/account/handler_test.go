eb53c793ab0cb01e64325289f2ecc7c33f388ea2
Anish Liu <jk5605012@gmail.com>
2020-08-05 21:11:21 +0800

    dddd

--- models/account/handler_test.go
+++ models/account/handler_test.go
@@ -30,7 +30,7 @@ func TestAccounts_InsertNewAccount(t *testing.T) {
 			wantErr: false,
 		},
 	}
-	pool, err := dockertest.NewPool("http://localhost:2375")
+	pool, err := dockertest.NewPool("tcp://localhost:2375")
 	if err != nil {
 		log.Fatalf("Could not connect to docker: %s", err)
 	}
